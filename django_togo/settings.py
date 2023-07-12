7953a6dead38af6f49c44df7f23a2acbccac0792
choyoon88 <azureluster@gmail.com>
2023-07-12 11:09:55 +0000

    commited first django project

--- django_togo/settings.py
+++ django_togo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-choyoon88-hellodjango-i5axa06u8bi.ws-eu101.gitpod.io']
 
 
 # Application definition
